589c280259b789b5cd0cd7cd3c02af2e96a7855c
hupe1980 <hupe@Franks-MBP-2.fritz.box>
2019-05-10 22:39:59 +0200

    feat: Add prefixing and minification

--- src/gatsby-ssr.js
+++ src/gatsby-ssr.js
@@ -1,7 +1,19 @@
+import React from 'react';
 import { ServerStyleSheets } from '@material-ui/styles';
+import postcss from 'postcss';
+import autoprefixer from 'autoprefixer';
+import CleanCSS from 'clean-css';
 
 // Keep track of sheets for each page
 const globalLeak = new Map();
+
+const prefixer = postcss([autoprefixer]);
+const cleanCSS = new CleanCSS();
+
+const defaultOptions = {
+  disableAutoprefixing: false,
+  disableMinification: false,
+};
 
 export const wrapRootElement = ({ element, pathname }, pluginOptions) => {
   const sheets = new ServerStyleSheets(pluginOptions.stylesProvider);
@@ -10,10 +22,32 @@ export const wrapRootElement = ({ element, pathname }, pluginOptions) => {
   return sheets.collect(element);
 };
 
-export const onRenderBody = ({ setHeadComponents, pathname }) => {
+export const onRenderBody = (
+  { setHeadComponents, pathname },
+  pluginOptions,
+) => {
   const sheets = globalLeak.get(pathname);
+
   if (sheets) {
-    setHeadComponents([sheets.getStyleElement()]);
+    const { disableAutoprefixing, disableMinification } = {
+      ...defaultOptions,
+      ...pluginOptions,
+    };
+
+    let css = sheets.toString();
+
+    css = disableAutoprefixing ? css : prefixer.process(css).css;
+    css = disableMinification ? css : cleanCSS.minify(css).styles;
+
+    setHeadComponents([
+      <style
+        type="text/css"
+        id="server-side-jss"
+        key="server-side-jss"
+        dangerouslySetInnerHTML={{ __html: css }}
+      />,
+    ]);
+
     globalLeak.delete(pathname);
   }
 };
