486aeb73e985172143c17f13b55f0e7396b84ecf
Magnus Rinnan Gaarder <magnus.rinnan@gmail.com>
2019-06-10 00:37:53 +0200

    fix: fix test, modify conditional in ssr and browser

--- e2e-tests/production-runtime/gatsby-config.js
+++ e2e-tests/production-runtime/gatsby-config.js
@@ -5,9 +5,9 @@ module.exports = {
       resolve: `gatsby-plugin-material-ui`,
       // If you want to use styled components you should change the injection order.
       options: {
-        // stylesProvider: {
-        //   injectFirst: true,
-        // },
+        stylesProvider: {
+          //   injectFirst: true,
+        },
       },
     },
     // If you want to use styled components you should add the plugin here.
--- gatsby-plugin-material-ui/.cache/styles-provider-props.js
+++ gatsby-plugin-material-ui/.cache/styles-provider-props.js
@@ -1,5 +1,3 @@
-const stylesProviderProps = {
-injectFirst: true,
-};
+const stylesProviderProps = {};
 
 export default stylesProviderProps;
