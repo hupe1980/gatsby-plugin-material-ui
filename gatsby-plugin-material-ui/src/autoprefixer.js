cbd3cf265a126c871b4b9837d1672873c5c4e2fb
Denis Augsburger <d.augsbi@gmail.com>
2021-03-29 23:11:56 +0200

    feat: Upgrade to Gatsby V3 (#66)
    * Try with browserslist provided by gatsby
    
    * Remove added peerdependency, included in subpackage
    
    * Import from utils
    
    * Load browserslist config manually
    
    * Try to dynamically load browserslistrc
    
    * Remove loading from .browserslistrc
    
    * Load browserslist config via babel-preset-gatsby
    Upgrade dependencies
    
    * Add browerslist from package.json
    
    * chore: Update node version in ci
    
    * Fix tests
    
    * Ugrade dependencies in root
    
    Co-authored-by: hupe1980 <frankhuebner1980@gmail.com>

--- gatsby-plugin-material-ui/src/autoprefixer.js
+++ gatsby-plugin-material-ui/src/autoprefixer.js
@@ -1,11 +1,18 @@
 import postcss from "postcss";
 import autoprefixer from "autoprefixer";
+import { browserslist } from "/package.json";
 
 export default function(css, pathname) {
-  const prefixer = postcss([autoprefixer]);
+  const prefixer = postcss([
+    autoprefixer({
+      overrideBrowserslist: browserslist
+        ? browserslist
+        : [`>0.25%`, `not dead`],
+    }),
+  ]);
 
   try {
-    return prefixer.process(css, { from: undefined }).css;
+    return prefixer.process(css, { from: null }).css;
   } catch (error) {
     if (error.name === `CssSyntaxError`) {
       throw new Error(`Pathname: ${pathname} ${error.toString()}`);
